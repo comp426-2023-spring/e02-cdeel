7a983e27bd46320ed729a8dc6223d6ed5aa57a42
cdeel <carson.deel@gmail.com>
2023-05-08 20:51:18 -0400

    first

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,39 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+async function playGame() {
+    var game = Array.from(document.getElementByName("game_select")).find(mode => mode.checked)?.value;
+    var shot = document.getElementById("myDropdown").value;
+
+    if (document.getElementById("opponent").checked) {
+        const url = `/app/${game}/play/${shot}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        document.getElementById("result").textContent = "You shot ${data.player} and ${data.result} against ${data.opponent}";
+    } else {
+        const url = `/app/${game}/play/${""}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        document.getElementById("result").textContent = "You shot ${data.player}";
+    }
+
+}
+
+function updateOptions() {
+    var radio1 = document.getElementById("rps_button");
+    var radio2 = document.getElementById("rpsls_button");
+    var dropdown = document.getElementById("myDropdown");
+    if (radio1.checked) {
+        dropdown.options.length = 0; // clear existing options
+        dropdown.options[dropdown.options.length] = new Option("rock");
+        dropdown.options[dropdown.options.length] = new Option("paper");
+        dropdown.options[dropdown.options.length] = new Option("scissors");
+    } else if (radio2.checked) {
+        dropdown.options.length = 0; // clear existing options
+        dropdown.options[dropdown.options.length] = new Option("rock");
+        dropdown.options[dropdown.options.length] = new Option("paper");
+        dropdown.options[dropdown.options.length] = new Option("scissors");
+        dropdown.options[dropdown.options.length] = new Option("lizard");
+        dropdown.options[dropdown.options.length] = new Option("spock");
+    }
+}
--- server.js
+++ server.js
@@ -102,3 +102,30 @@ process.on('SIGINT', () => {
         }    
     })
 })
+
+import express from 'express';
+import minimist from 'minimist';
+import { rps, rpsls } from './lib/rpsls.js';
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/app', (req, res) => {res.status(200).send("200 OK");})
+
+app.get('/app/rpsls', (req, res) => {res.status(200).send(rpsls());})
+
+app.get('/app/rps/play', (req, res) => {res.status(200).send(rps(req.query.shot));})
+
+app.get('/app/rpsls/play', (req, res) => {res.status(200).send(rpsls(req.query.shot));})
+
+app.post('/app/rps/play', (req, res) => {res.status(200).send(rps(req.body.shot));})
+
+app.post('/app/rpsls/play', (req, res) => {res.status(200).send(rpsls(req.body.shot));})
+
+app.get('/app/rps/play/:arg', (req, res) => {res.status(200).send(rps(req.params.arg));})
+
+app.get('/app/rpsls/play/:arg', (req, res) => {res.status(200).send(rpsls(req.params.arg));})
+
+app.get('*', (req, res) => {res.status(404).send('404 NOT FOUND');})
+
+app.listen(port, () => {console.log(`App listening on port ${port}`);})
